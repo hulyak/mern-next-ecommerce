2e9f7cf73b8e0dc25fd3f4531622c92e60ec658a
hulya <hulyakarakayaa06@gmail.com>
2021-02-04 11:24:16 -0800

    create the cart page

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,5 +1,22 @@
+import { Header, Segment, Button, Icon } from 'semantic-ui-react';
+
 function CartItemList() {
-  return <>CartItemList</>;
+  const user = true;
+  return (
+    <Segment secondary color='yellow' inverted textAlign='center' placeholder>
+      <Header icon>
+        <Icon name='shopping basket' />
+        No Products in your cart. Add Some!
+      </Header>
+      <div>
+        {user ? (
+          <Button color='orange'>View Products</Button>
+        ) : (
+          <Button color='olive'>Login to Add Products</Button>
+        )}
+      </div>
+    </Segment>
+  );
 }
 
 export default CartItemList;
--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,5 +1,15 @@
+import { Button, Divider, Segment } from 'semantic-ui-react';
+
 function CartSummary() {
-  return <>CartSummary</>;
+  return (
+    <>
+      <Divider />
+      <Segment clearing size='large'>
+        <strong>Subtotal: </strong> $0.00
+        <Button icon='cart' color='yellow' floated='right' content='Checkout' />
+      </Segment>
+    </>
+  );
 }
 
 export default CartSummary;
