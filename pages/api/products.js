8468fdd13c2d8e72f1e6af7478e6a515e099b8aa
hulya <hulyakarakayaa06@gmail.com>
2021-02-05 13:02:59 -0800

    add pagination for product list home page

--- pages/api/products.js
+++ pages/api/products.js
@@ -5,7 +5,18 @@ import Product from '../../models/Product';
 connectDb();
 
 export default async (req, res) => {
-  // console.log(req.method) // GET
-  const products = await Product.find();
-  res.status(200).json(products);
+  const { page, size } = req.query;
+  // convert querystring values to numbers
+  const pageNum = Number(page);
+  const pageSize = Number(size);
+  let products = [];
+  const totalDocs = await Product.countDocuments();
+  const totalPages = Math.ceil(totalDocs / pageSize);
+  if (pageNum === 1) {
+    products = await Product.find().limit(pageSize);
+  } else {
+    const skips = pageSize * (pageNum - 1);
+    products = await Product.find().skip(skips).limit(pageSize);
+  }
+  res.status(200).json({ products, totalPages });
 };
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
+import ProductPagination from '../components/Index/ProductPagination';
 import Meta from '../components/_App/Meta';
 import baseUrl from '../utils/baseUrl';
 
-function Home({ products }) {
+function Home({ products, totalPages }) {
   return (
     <>
       <Meta />
       <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
     </>
   );
 }
 
 export async function getServerSideProps(context) {
+  const page = context.query.page ? context.query.page : '1';
+  const size = 9;
   const url = `${baseUrl}/api/products`;
-  const res = await axios.get(url);
-  return { props: { products: res.data } };
+  const payload = { params: { page, size } };
+  const res = await axios.get(url, payload);
+  return { props: res.data };
 }
 
 export default Home;
